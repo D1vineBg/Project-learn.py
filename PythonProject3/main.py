32f2aed94376b92e0e17d15b9994983e20614c04
D1vineBg <yrlnel28@gmail.com>
2025-02-05 18:34:59 +0300

    Add files via upload
    This was my first day of learning python.I was watchng tutorials while writing and coding.I always tried things when i didnt understand.After the session i felt sooo smart.

---
+++ PythonProject3/main.py
@@ -0,0 +1,48 @@
+print("hello")
+firstName=20
+secondName=10
+print("firstName*secondName")
+friends=10
+friends=friends//2
+print(10*2)
+print("friends+10")
+print(friends+100)
+#btw this is my first python code
+#notes:
+#variable=A reusable container for a value(string,intergers,float,boolean)
+#the variable behaves as if it was the value that it contains
+#examples
+
+full_name = "Neil Okita"
+
+print(f"hello  {full_name} ")
+
+age=25
+
+gpa=3.2
+print(f"Your gpa is {gpa}")
+
+is_student=False
+print(f"Are you a student?:{is_student}")
+
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+
+
+
+name=input("Enter your name: ")
+
+print(f"Hello{name}!")
+
+age=int(input("Enter your age"))
+print(f"You are{age} years old!")
+if age >=18:
+    print("You are an adult!")
+else:
+    print("You are NOT an adult")
+
+
